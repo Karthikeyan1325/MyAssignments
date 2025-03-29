2bc71da725183f6202bf8192debfa9571473f3f9
Karthikeyan1325 <mjkarthikeyan1998@gmail.com>
2025-03-29 16:58:58 +0530

    Add files via upload
    Day 1 Assignment

---
+++ Chrome.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 float version = 91.0f;
+		 System.out.println(version);
+		 
+		 String google = "Google";
+		 System.out.println(google);
+		 
+		 boolean beta = false;
+		 System.out.println(beta);
+		 
+		 int releaseYear = 2008;
+		 System.out.println(releaseYear);
+		 
+		 char shortCutKey = 'c';
+		 System.out.println(shortCutKey);
+		
+
+	}
+
+}
