0e728f2f7e67e14991067fae37941805320b2944
Karthikeyan1325 <mjkarthikeyan1998@gmail.com>
2025-04-05 16:40:57 +0530

    Add files via upload

---
+++ LibraryManagement.java
@@ -0,0 +1,16 @@
+package week2.day1;
+
+public class LibraryManagement {
+	
+	public static void main(String[] args) {
+		
+		
+		LibraryClass book = new LibraryClass();
+		
+		book.addBook("Book added successfully");
+		
+		
+		book.issueBook();
+	}
+
+}
