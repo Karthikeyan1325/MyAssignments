0e3a24c900cdca1928b5573e9f8532ea9589fefb
Karthikeyan1325 <mjkarthikeyan1998@gmail.com>
2025-04-19 17:42:10 +0530

    Add files via upload

---
+++ MySqlConnection.java
@@ -0,0 +1,11 @@
+package week4.day1;
+
+public abstract class MySqlConnection implements DatabaseConnection {
+	
+	public void executeQuery() {
+		// TODO Auto-generated method stub
+
+		System.out.println("Execute the query in the database");
+	}
+
+}
