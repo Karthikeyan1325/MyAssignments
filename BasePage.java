831ee171cd4d9bcdba79fa4bd2ffe43e17f9e420
Karthikeyan1325 <mjkarthikeyan1998@gmail.com>
2025-04-19 17:35:45 +0530

    Add files via upload

---
+++ BasePage.java
@@ -0,0 +1,34 @@
+package week4.day1;
+
+public class BasePage {
+
+	
+	
+	public void findElement() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Find the element of the web page");
+
+	}
+	
+	public void clickElement() {
+		// TODO Auto-generated method stub
+		System.out.println("click the element of the web page");
+
+	}
+	
+	public void enterText() {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the value in the web page");
+
+	}
+	
+	public void performCommonTasks() {
+		// TODO Auto-generated method stub
+		System.out.println("Peform an operation in the web page");
+
+	}
+	
+	
+	
+}
